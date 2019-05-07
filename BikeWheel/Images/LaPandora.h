b5c2081d6cd8c115adaf8be01186211f134b2c35
Peter Riesz <pete@visualeyes.net.au>
2019-05-07 13:50:52 +0200

    move space instead instead of rocket

--- BikeWheel/Images/LaPandora.h
+++ BikeWheel/Images/LaPandora.h
@@ -17,15 +17,15 @@ public:
     void render(uint16_t zero_angle, int32_t rotation_rate) {
         bool reverse = rotation_rate < 0;
 
-        uint16_t rocket_angle = zero_angle + (millis() << 2);
+        uint16_t space_angle = zero_angle - (millis() << 2);
 
         for (uint8_t i = 0; i < NUM_PIXELS; i++) {
             uint16_t led_angle = Leds::get_angle(i);
-            uint16_t angle = rocket_angle + led_angle;
+            uint16_t angle = zero_angle + led_angle;
             uint32_t color = rocket_img.get_led_color(i, reverse ? ~angle : angle);
 
             if (color == 0) {
-                angle = zero_angle + led_angle;
+                angle = space_angle + led_angle;
                 color = space_img.get_led_color(i, reverse ? ~angle : angle);
             }
 
